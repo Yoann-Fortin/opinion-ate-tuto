004ae7b10bf28b596a5c246c7a5af0f3716c99dc
Yoann Fortin <yoannfortin13@gmail.com>
2021-01-30 09:26:22 +0100

    Load restaurants upon first rendering RestaurantList

---
+++ src/components/RestaurantList.js
@@ -0,0 +1,11 @@
+import React, {useEffect} from 'react';
+
+export const RestaurantList = ({loadRestaurants}) => {
+    useEffect(() => {
+        loadRestaurants();
+    }, [loadRestaurants]);
+    
+    return <div>RestaurantList</div>;
+};
+
+export default RestaurantList;
