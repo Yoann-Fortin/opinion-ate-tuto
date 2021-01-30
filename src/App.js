f689152580ade5ee95a89ee13e7687080b31e351
Yoann Fortin <yoannfortin13@gmail.com>
2021-01-30 10:06:28 +0100

    All tests passed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import {Provider} from 'react-redux';
+import store from './store';
+
 import RestaurantScreen from './components/RestaurantScreen';
 
 const App = () => (
-  <div>
+  <Provider store={store}>
     <RestaurantScreen />
-  </div>
+  </Provider>
 );
 
 export default App;
